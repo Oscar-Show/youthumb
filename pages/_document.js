3bc817f26c8aed1b8646c72553ee142638be6792
Oscar-Show <brahim.ecom1990@gmail.com>
2023-08-25 01:52:58 +0200

    privacy policy

--- pages/_document.js
+++ pages/_document.js
@@ -140,7 +140,7 @@ class MyDocument extends Document {
             </div>
             <div>
               <a href="/terms">Terms of Service</a> |{" "}
-              <a href="/Privacy Policy">Privacy Policy</a>
+              <a href="pages/Privacy Policy">Privacy Policy</a>
             </div>
           </footer>
         </body>
